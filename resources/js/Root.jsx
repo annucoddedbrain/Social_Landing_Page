de87096343df4c30ae83d4c3b691159c92c2058a
AzzadCodeBrain <azzad@coddedbrain.com>
2023-09-29 15:33:37 +0530

    Page 3 Done

--- resources/js/Root.jsx
+++ resources/js/Root.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Page1 from "./pages/Landing/Page1";
 import Page2 from "./pages/Landing/Page2";
+import Page3 from "./pages/Landing/Page3";
 
 function Root(){
 
@@ -11,6 +12,7 @@ function Root(){
             <Routes>
                 <Route path='/' exact={true} element={<Page1 />}/>
                 <Route path='/page-2' exact={true} Component={Page2}/>
+                <Route path='/page-3' exact={true} Component={Page3}/>
             </Routes>
         </>
     )

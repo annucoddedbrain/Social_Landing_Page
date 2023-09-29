5c3bc945f31615b6c2782f7b8f65b1e0ba0554f0
AzzadCodeBrain <azzad@coddedbrain.com>
2023-09-29 17:35:40 +0530

    Done Page6

--- resources/js/Root.jsx
+++ resources/js/Root.jsx
@@ -4,6 +4,7 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Page1 from "./pages/Landing/Page1";
 import Page2 from "./pages/Landing/Page2";
 import Page3 from "./pages/Landing/Page3";
+import Page6 from "./pages/Landing/Page6";
 
 function Root(){
 
@@ -13,6 +14,7 @@ function Root(){
                 <Route path='/' exact={true} element={<Page1 />}/>
                 <Route path='/page-2' exact={true} Component={Page2}/>
                 <Route path='/page-3' exact={true} Component={Page3}/>
+                <Route path='/page-6' exact={true} Component={Page6}/>
             </Routes>
         </>
     )
---
+++ resources/js/pages/Landing/Page6.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.scss";
+import { Button } from "react-bootstrap";
+
+export default function Page6(){
+    return(
+        <>
+        <h3 className="text-center w-50 m-auto p-5 mt-5 fs-2 fw-bold">Ready to Grow Together?</h3>
+
+        <h3 className="text-center w-50 m-auto p-5 fs-2 fw-bold" >If you're ready to take your audience growth to the next level, you're in the right place. Let's dive in and start building something amazing, together!"</h3>
+
+        <Button className="shadow border-0 rounded-3 fs-4 fw-bold Started">Get Started Now</Button>
+        </>
+    )
+}
